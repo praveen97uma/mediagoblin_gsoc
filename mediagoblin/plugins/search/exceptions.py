3499dba425346985faa7f60bfaa1c88aaf5484f1
Praveen Kumar <praveen97uma@gmail.com>
2013-07-02 21:32:45 +0530

    Refactor exceptions raised by the search plugin

--- mediagoblin/plugins/search/exceptions.py
+++ mediagoblin/plugins/search/exceptions.py
@@ -8,16 +8,12 @@ class BaseSearchError(Exception):
     def __init__(self, msg=None):
         self.msg = msg
 
-    def __str__(self):
-        return repr(self.msg)
-
-
 class IndexDoesNotExistsError(BaseSearchError):
     """
     Raised when an index does not exist.
     """
 
-    def __init__(self, index_dir, index_name):
-        self.msg = ('Index with name %s does not exists in search', 
-                    'index directory %s'%(index_dir, index_name))
-
+    def __init__(self, msg='', index_dir='', index_name=''):
+        super(IndexDoesNotExistsError, self).__init__(msg)
+        message = 'Index with name %(name)s does not exists in search index %(dir)s'
+        self.msg = message.format({'name': index_name, 'dir': index_dir})
