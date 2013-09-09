8776f3394183bf62983fed96ddd20386cb74e09b
Praveen Kumar <praveen97uma@gmail.com>
2013-09-10 00:29:21 +0530

    Add a constants.py file to store index identifiers.

---
+++ mediagoblin/plugins/search/constants.py
@@ -0,0 +1,13 @@
+from mediagoblin.db.models import (MediaEntry, MediaTag)
+# List of index identifiers
+MEDIA_ENTRIES = "media_entries"
+
+MEDIA_TAGS = "media_tags"
+
+
+ModelIndexMapping = {
+    MediaEntry.__tablename__: MEDIA_ENTRIES,
+    MediaTag.__tablename__: MEDIA_TAGS,
+}
+
+
